fd36ab117d77c9a76a6828206058863395d65f85
Ryan Gragg <rygragg@gmail.com>
2024-08-20 22:55:15 -0700

    idk

--- 2/a.hs
+++ 2/a.hs
@@ -1,8 +1,8 @@
 import qualified Data.Text    as Text
 import qualified Data.Text.IO as Text
 
--- f :: [String] -> [Int]
--- f strs = filter (\x -> x !! 0 == 'f') strs
+f :: [String] -> [String]
+f strs = filter (\x -> head x == 'f') strs
 
 -- d :: [String] -> [Int]
 -- d strs = filter (\x -> x !! 0 == 'd') strs
@@ -14,11 +14,11 @@ import qualified Data.Text.IO as Text
 -- solve strs = (f strs) * ((d strs) - (u strs))
 
 textToStrs :: [Text.Text] -> [String]
-textToStrs = (map read::String)
+textToStrs = map (read . Text.unpack)
 
 main :: IO ()
 main = do
     ls <- fmap Text.lines (Text.readFile "test.txt")
     let input = textToStrs ls
     -- print (solve input)
-    print(ls)
+    print(f input)
