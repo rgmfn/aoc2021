e0416a02237b44c1b9ca322bc4d71f43ce7e41e7
Ryan Gragg <rygragg@gmail.com>
2024-08-25 11:24:04 -0700

    2a, gitignore

--- 2/a.hs
+++ 2/a.hs
@@ -1,24 +1,22 @@
 import qualified Data.Text    as Text
 import qualified Data.Text.IO as Text
+import Data.Char (digitToInt)
 
-f :: [String] -> [String]
-f strs = filter (\x -> head x == 'f') strs
-
--- d :: [String] -> [Int]
--- d strs = filter (\x -> x !! 0 == 'd') strs
-
--- u :: [String] -> [Int]
--- u strs = filter (\x -> x !! 0 == 'u') strs
-
--- solve :: [String] -> Int
--- solve strs = (f strs) * ((d strs) - (u strs))
+-- 
+solve :: Int -> Int -> Int -> [String] -> Int
+solve d u f [] = f * (d - u)
+solve d u f (x:xs) =
+    let amt = digitToInt (last x)
+    in case head x of
+        'd' -> solve (d+amt) u f xs
+        'u' -> solve d (u+amt) f xs
+        'f' -> solve d u (f+amt) xs
 
 textToStrs :: [Text.Text] -> [String]
-textToStrs = map (read . Text.unpack)
+textToStrs = map Text.unpack
 
 main :: IO ()
 main = do
-    ls <- fmap Text.lines (Text.readFile "test.txt")
+    ls <- fmap Text.lines (Text.readFile "input.txt")
     let input = textToStrs ls
-    -- print (solve input)
-    print(f input)
+    print(solve 0 0 0 input)
